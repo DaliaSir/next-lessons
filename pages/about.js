3abf5de70a304502c987ef907a92bc7a0dcb29d9
DaliaSir <66668341+DaliaSir@users.noreply.github.com>
2021-12-07 14:42:00 +0100

    First commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head title="About" />
+
+      <div className="container">
+        <h1>About page</h1>
+      </div>
+    </Layout>
+  );
+}
